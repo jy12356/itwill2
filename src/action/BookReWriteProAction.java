7d427fc222a5a06421a777a74cb59e9fd5a70d8d
ITWILL <ITWILL@DESKTOP-6181D4M>
2020-11-17 19:45:07 +0900

    review

--- src/action/BookReWriteProAction.java
+++ src/action/BookReWriteProAction.java
@@ -1,5 +1,6 @@
 package action;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,7 +10,15 @@ public class BookReWriteProAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return null;
+		System.out.println("BookReWriteProAction!");
+		
+		ActionForward forward = null;
+		
+		// 현재 컨텍스트(객체) 정보 가져오기 위해  request 객체의 getServletContext() 메서드를 호출
+		ServletContext context = request.getServletContext();
+		
+		
+		return forward;
 	}
 
 }
