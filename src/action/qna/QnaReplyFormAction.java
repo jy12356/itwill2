3f609c60f109d6a13369027101de8188ad61fd65
ITWILL <ITWILL@DESKTOP-6181D4M>
2020-11-26 16:55:18 +0900

    20201126

--- src/action/qna/QnaReplyFormAction.java
+++ src/action/qna/QnaReplyFormAction.java
@@ -4,14 +4,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import action.Action;
+import svc.qna.QnaDetailService;
 import vo.ActionForward;
+import vo.QnaBean;
 
 public class QnaReplyFormAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("QnaReplyFormAction - ActionForward");
+		
+		ActionForward forward = null;
+		int board_num = Integer.parseInt(request.getParameter("board_num"));
+		
+		QnaDetailService qnaDetailService = new QnaDetailService();
+		QnaBean article = qnaDetailService.getArticle(board_num);
+		
+		request.setAttribute("article", article);
+		
+		forward = new ActionForward();
+		forward.setPath("/sub5/qna5.jsp");
+		
+		return forward;
 	}
 
 }
