6cdac3d53998e25112ebdb7b847e03ac7c6225c9
ReadStar <ReadStar@users.noreply.github.com>
2020-11-27 13:48:48 +0900

    Notice

---
+++ src/vo/NoticeBean.java
@@ -0,0 +1,58 @@
+package vo;
+
+import java.sql.Date;
+
+public class NoticeBean {
+	private int num;
+	private String subject;
+	private String id;
+	private String content;
+	private String file;
+	private Date date;
+	private String kind;
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+}
+	
