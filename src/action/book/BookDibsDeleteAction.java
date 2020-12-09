9940f0608514a7b54a1c01389d64de8bbfe8f96d
jy12356 <72728986+jy12356@users.noreply.github.com>
2020-12-10 00:05:13 +0900

    Add files via upload

---
+++ src/action/book/BookDibsDeleteAction.java
@@ -0,0 +1,51 @@
+package action.book;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import action.Action;
+import svc.book.BookDeleteProService;
+import svc.book.BookDibsDeleteService;
+import vo.ActionForward;
+
+public class BookDibsDeleteAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ActionForward forward = null;
+
+		String id = request.getParameter("id");
+		int num = Integer.parseInt(request.getParameter("num"));
+		
+		BookDibsDeleteService bookDibsDeleteService = new BookDibsDeleteService(); 
+		boolean isBookDel =  bookDibsDeleteService.dibsDelete(id,num);
+		if(!isBookDel) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('이미 삭제되었습니다.')");
+			out.println("history.back()");
+			out.println("</script>");
+		}else {
+			boolean isDelete = bookDibsDeleteService.dibsDelete(id,num);
+			if(!isDelete) {
+				response.setContentType("text/html; charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println("<script>");
+				out.println("alert('삭제에 실패하였습니다.')");
+				out.println("history.back()");
+				out.println("</script>");
+			}else {
+				forward = new ActionForward();
+				forward.setPath("BookDibsList.bok");
+				forward.setRedirect(true);
+			}
+		}
+		
+		return forward;
+	}
+
+
+}
